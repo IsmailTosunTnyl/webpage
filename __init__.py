045fcc93fc3d8f694102a73bc0eed3db78f35b38
ismail <ismail19991999@gmail.com>
2021-09-02 10:51:23 +0300

    index and about

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-from dotenv import dotenv_values , set_key
+from dotenv import dotenv_values, set_key
 
 env = dotenv_values(".env")
 
 app = Flask(__name__)
 app.debug = True
-app.config["MYSQL_HOST"]= env["MYSQL_HOST"]
+app.config["MYSQL_HOST"] = env["MYSQL_HOST"]
 app.config["MYSQL_USER"] = env["MYSQL_USER"]
 app.config["MYSQL_PASSWORD"] = env["MYSQL_PASSWORD"]
 app.config["MYSQL_DB"] = env["MYSQL_DB"]
@@ -15,19 +15,27 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
 
-
-
 @app.route('/')
 def index():  # put application's code here
 
-    print(request.accept_languages.best_match(["de","tr"]))
+    print(request.accept_languages.best_match(["de", "tr"]))
     cursor = mysql.connection.cursor()
     print(cursor.execute("SELECT * from languages"))
     data = cursor.fetchall()
+    cursor.close()
 
-    return render_template("index.html",data = data[0])
+    return render_template("index.html", data=data[0])
+
+
+@app.route("/about")
+def about():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * from languages")
+    data = cursor.fetchall()
+    cursor.close()
+
+    return render_template("about.html", data=data[0])
 
 
 if __name__ == '__main__':
     app.run()
-
